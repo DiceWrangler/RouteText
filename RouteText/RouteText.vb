d778532985f7f3fde9af755085752f1e7bc35424
Scott Thorne <dicewrangler@gmail.com>
2018-09-05 19:00:21 -0600

    update version number

--- RouteText/RouteText.vb
+++ RouteText/RouteText.vb
@@ -14,7 +14,7 @@ Module RouteText
     Public gReportItemsFolderName As String
 
     Public Const APP_NAME As String = "RouteText"
-    Const APP_VERSION As String = "v180903"
+    Const APP_VERSION As String = "v180905"
 
     Public Structure EmailMessage
 
